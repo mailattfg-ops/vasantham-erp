b56e9206fb966379b8b4f35c27c41178fc27c266
shihadkp <mshihadkp7@gmail.com>
2026-04-27 15:26:12 +0530

    Fix: Adjust layout margin for sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -27,7 +27,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="flex h-screen overflow-hidden bg-background">
       <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden" style={{ marginLeft: '240px' }}>
+      <div className="flex flex-col flex-1 overflow-hidden" style={{ marginLeft: '250px' }}>
         <Header />
         <main className="flex-1 overflow-y-auto p-6">
           {children}
